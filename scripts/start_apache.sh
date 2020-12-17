dfac3499495f2647f6b4609f5e46f2ad5336d65c
rahul-8901 <52934279+rahul-8901@users.noreply.github.com>
2020-12-17 12:35:27 +0530

    Update start_apache.sh

--- scripts/start_apache.sh
+++ scripts/start_apache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # starting service
 cd /var/www/html
-mv /var/www/html/dist/discogs-api-client/* /var/www/html/
 service httpd start
+rm -rf /opt/codedeploy-agent/deployment-root/41ec0214-79c8-409a-803f-f10425871e03/*
